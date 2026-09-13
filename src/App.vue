c6cce1dc6a8a4dfe0e3a3f96dd2a1a687dece427
Mohamed Helmy <mohhelmy@students.du.edu.eg>
2026-09-14 00:24:26 +0300

    Wire up Navbar component in App

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+import Navbar from './Components/Navbar.vue'
 import SubscribeSection from './Components/SubscribeSection.vue'
 import Footer from './Components/Footer.vue'
 </script>
 
 <template>
+  <Navbar />
   <main>
     <SubscribeSection />
     <router-view />
