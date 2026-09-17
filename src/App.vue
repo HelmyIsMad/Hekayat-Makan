85184f8bb26ae96f9f57e638b7dae9ebb08509fe
Rahma Reda <rahma27r4@gmail.com>
2026-09-17 15:04:40 +0300

    Fix place details page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
 import Navbar from './Components/Navbar.vue'
-// import HeroSection from './Components/HeroSection.vue'
 import SubscribeSection from './Components/SubscribeSection.vue'
 import Footer from './Components/Footer.vue'
+
+const route = useRoute()
+
+// رحمة: نخفي الاشتراك والفوتر من صفحة تفاصيل المكان
+const isPlaceDetail = computed(() => {
+  return route.name === 'PlaceDetail'
+})
 </script>
 
 <template>
   <Navbar />
+
   <main>
-    <!-- <HeroSection /> -->
     <router-view />
-    <SubscribeSection />
-    
+
+    <!-- رحمة: يظهر الاشتراك في باقي الصفحات فقط -->
+    <SubscribeSection v-if="!isPlaceDetail" />
   </main>
 
-  <Footer />
+  <!-- رحمة: الفوتر يظهر في باقي الصفحات فقط -->
+  <Footer v-if="!isPlaceDetail" />
 </template>
